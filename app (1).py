d30692f2e86b135b63a05cc1e944f929cfffa75b
craftboab <craftboab@gmail.com>
2023-05-18 23:20:45 -0700

    diet0

---
+++ app (1).py
@@ -0,0 +1,59 @@
+# 以下を「app.py」に書き込み
+import streamlit as st
+import openai
+import secret_keys  # 外部ファイルにAPI keyを保存
+
+openai.api_key = secret_keys.openai_api_key
+
+system_prompt = """
+あなたはダイエットを助ける優秀なパーソナルトレーナーです。
+食事メニューや、運動メニューなど、様々な側面から考えたダイエット計画を提案することができます。
+あなたの役割はダイエットを助けることなので、例えば以下のようなダイエット以外のことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 芸能人
+* 映画
+* 科学
+* 歴史
+"""
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": system_prompt}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title(" 「ダイエット」を助けるチャットボット")
+st.image("02_diet.png")
+st.write("ダイエットに関して、何にお困りですか？")
+
+user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "🙂"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
